3aa5a510d710c27636fd8fc8ee09b233b6f2d44a
Kevin Schulthess <kevin_schulthess@bluewin.ch>
2023-03-23 14:16:58 +0100

    Add files via upload

---
+++ vcid/base.py
@@ -0,0 +1,17 @@
+from dynaconf import FlaskDynaconf
+from flask import Flask
+
+
+def create_app(**config):
+    app = Flask(__name__)
+    FlaskDynaconf(app)  # config managed by Dynaconf
+    app.config.load_extensions(
+        "EXTENSIONS"
+    )  # Load extensions from settings.toml
+    app.config.update(config)  # Override with passed config
+    return app
+
+
+def create_app_wsgi():
+    app = create_app()
+    return app
